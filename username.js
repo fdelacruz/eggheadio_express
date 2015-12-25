fd5b3a0d414578dcd823445f594609ca28de0a1d
Francisco De La Cruz <helloquico@gmail.com>
2015-12-24 22:21:00 -0500

    Middleware
    Featuring Error Handling Middleware

--- username.js
+++ username.js
@@ -6,18 +6,25 @@ var router = express.Router({
 	mergeParams: true
 });
 
-router.all('/', function (req, res, next) {
-	console.log(req.method, 'for', req.params.username);
+router.use(function (req, res, next) {
+	console.log(req.method, 'for', req.params.username, ' at ' + req.path);
 	next();
 });
 
-router.get('/', helpers.verifyUser, function (req, res) {
+// delete helpers.verifyUser to trigger error
+router.get('/', function (req, res) {
 	var username = req.params.username;
 	var user = helpers.getUser(username);
 	res.render('user', {
 		user: user,
 		address: user.location
 	});
+});
+
+// error handling middleware
+router.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
 });
 
 router.put('/', function (req, res) {
